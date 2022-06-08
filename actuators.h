4db2a103329961d50143a06d130cccfe068cbaa4
Wildan Gunawan <35158077+wildangunawan@users.noreply.github.com>
2022-06-08 08:08:15 +0700

    fix: declare function

--- actuators.h
+++ actuators.h
@@ -1,8 +1,4 @@
 #include "Arduino.h"
 
 void setupActuators();
-void maju(int speedA = 100, int speedB = 100);
-void mundur(int speedA = 10, int speedB = 10);
-void berhenti();
-void belok_kiri(int speed = 10);
-void belok_kanan(int speed = 10);
+void controlSpeed(int speedL = 0, int speedR = 0);
